89169473b97ceab70d65d2e03da1d03a939d22fb
Martin Ganly <martinganly@gmail.com>
2018-02-07 12:05:56 +0000

    '07/02/18'

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-        return HttpResponse("Friend.")
+        context_dict = { 'boldmessage' : "Crunch, Munch, Lunch?"}
+        return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+        return HttpResponse("Here is the about page <a href='/'>Index</a>")
--- tango/urls.py
+++ tango/urls.py
@@ -19,9 +19,10 @@ from django.conf.urls import include
 from rango import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^rango/', include('rango.urls')),
+    url(r'^$', include('rango.urls')),
     # above maps any URLs starting with rango/ to be handled
     # by the rango application.
+    url(r'^$', views.index, name='index'),
+    url(r'^about/', views.about, name='about'),
     url(r'^admin/', admin.site.urls),
 ]
